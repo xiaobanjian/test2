e50607c2cc03f9360e5bb1fe9a1709aa5ca8a57d
wyb <845362531@qq.com>
2019-05-15 19:48:41 +0800

    修改

--- src/Test.java
+++ src/Test.java
@@ -2,6 +2,7 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("hi");
         System.out.println("hi2");
-
+        System.out.println("hi5");
+        System.out.println("hi6");
     }
 }
