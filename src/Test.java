3a5785f3094a86aec787ebfad02cea0f9ed83593
wyb <845362531@qq.com>
2019-05-15 19:46:35 +0800

    first修改

--- src/Test.java
+++ src/Test.java
@@ -2,6 +2,9 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("hi");
         System.out.println("hi2");
+        System.out.println("hi3");
+        System.out.println("hi4");
+
 
     }
 }
